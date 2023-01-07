b89ad1dcd4412a3ce6ed0072071a849d0577769a
Nishantjain10 <nishant.jain.775@gmail.com>
2023-01-08 01:46:47 +0530

    UI added userprofile page

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -2,7 +2,7 @@ import {Client, Account, Databases} from 'appwrite'
 const client = new Client();
 
 client
-    .setEndpoint('http://appwrite.vogueeco.us/v1')
+    .setEndpoint('https://appwrite.vogueeco.us/v1')
     .setProject('63aaeaf74d0ae48e48a5');
 
 export const account = new Account(client)
